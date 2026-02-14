d34778c77f97a4598496ca18d82e2991ddb6834e
Parham <parhamrayej138@gmail.com>
2026-02-14 20:12:30 +0330

    vaay

---
+++ phase-3.cpp
@@ -0,0 +1,10 @@
+#define SDL_MAIN_HANDLED
+#include <iostream>
+#include <SDL2/SDL.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "welcome to the project" << endl;
+    return 0;
+}
