7223dc1fd76f8b625133beb97e40094ccde4f66b
Abbas <amirabbassadeghi385@gmail.com>
2026-02-19 12:09:32 +0330

    refactor game loop to support time based update rate control

--- project/phase1.cpp
+++ project/phase1.cpp
@@ -1,4 +1,3 @@
-#define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 #include <iostream>
 
@@ -7,44 +6,77 @@
 
 using namespace std;
 
-bool is_running = true;
 
-void handle_events() {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT) {
-            is_running = false;
-        }
-        if (event.type == SDL_KEYDOWN) {
-            if (event.key.keysym.sym == SDLK_ESCAPE) {
-                is_running = false;
-            }
-        }
-    }
-}
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
 
-void update() {
+bool is_running = false;
 
-}
 
-void render(SDL_Renderer* renderer) {
-    SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
-    SDL_RenderClear(renderer);
+bool init();
+void handle_events();
+void update(float delta_time);
+void render();
+void clean();
 
-    SDL_Rect rect = {350, 250, 100, 100};
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &rect);
 
-    SDL_RenderPresent(renderer);
-}
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
 
-int main() {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        cout << "SDL init failed\n";
-        return 1;
+    if (!init())
+    {
+        cout << "Failed to initialize SDL!" << endl;
+        return -1;
     }
 
-    SDL_Window* window = SDL_CreateWindow(
+    const int target_fps = 60;
+    const float target_frame_time = 1.0f / target_fps;
+
+    float delta_time = 0.0f;
+    Uint32 last_ticks = 0;
+    Uint32 frame_start = 0;
+
+    last_ticks = SDL_GetTicks();
+    is_running = true;
+
+
+    while (is_running)
+    {
+        frame_start = SDL_GetTicks();
+
+
+        Uint32 current_ticks = SDL_GetTicks();
+        delta_time = (current_ticks - last_ticks) / 1000.0f;
+        last_ticks = current_ticks;
+
+        handle_events();
+        update(delta_time);
+        render();
+
+
+        Uint32 frame_time = SDL_GetTicks() - frame_start;
+        if (frame_time < (Uint32)(target_frame_time * 1000))
+        {
+            SDL_Delay((Uint32)(target_frame_time * 1000) - frame_time);
+        }
+    }
+
+    clean();
+    return 0;
+}
+
+
+bool init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        cout << "SDL_Init Error: " << SDL_GetError() << endl;
+        return false;
+    }
+
+    window = SDL_CreateWindow(
             "Scratch - Phase 1",
             SDL_WINDOWPOS_CENTERED,
             SDL_WINDOWPOS_CENTERED,
@@ -53,33 +85,75 @@ int main() {
             SDL_WINDOW_SHOWN
     );
 
-    if (!window) {
-        cout << "Window creation failed\n";
-        return 1;
+    if (!window)
+    {
+        cout << "Window Error: " << SDL_GetError() << endl;
+        return false;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(
+    renderer = SDL_CreateRenderer(
             window,
             -1,
-            SDL_RENDERER_ACCELERATED
+            SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
     );
 
-    if (!renderer) {
-        cout << "Renderer creation failed\n";
-        return 1;
+    if (!renderer)
+    {
+        cout << "Renderer Error: " << SDL_GetError() << endl;
+        return false;
     }
 
-    while (is_running) {
-        handle_events();
-        update();
-        render(renderer);
+    return true;
+}
 
-        SDL_Delay(16); // ~60 FPS
+
+void handle_events()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT)
+        {
+            is_running = false;
+        }
+        else if (event.type == SDL_KEYDOWN)
+        {
+            if (event.key.keysym.sym == SDLK_ESCAPE)
+            {
+                is_running = false;
+            }
+        }
     }
+}
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+
+void update(float delta_time)
+{
+    (void)delta_time;
+
+}
+
+
+
+void render()
+{
+    SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+    SDL_RenderClear(renderer);
+
+
+
+    SDL_RenderPresent(renderer);
+}
+
+
+
+void clean()
+{
+    if (renderer)
+        SDL_DestroyRenderer(renderer);
+
+    if (window)
+        SDL_DestroyWindow(window);
+
     SDL_Quit();
-
-    return 0;
 }
